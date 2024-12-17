53ef92c6960fb37a3d0d21a4ade9ad143303737c
Thao Nguyen <nguyen_huynh_thanh_thao@optimizer.co.jp>
2024-12-17 17:21:40 +0700

    finish logic

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -29,8 +29,16 @@ class EmployeesController < ApplicationController
     zipped_file = params[:file]
     if zipped_file.content_type == "application/zip"
       result_file = Employee.generate_summary(zipped_file)
-    else  
+      file_name = 'result.zip'
+      zipped_file_path = Rails.root.join('app', 'views', 'employees', 'review', file_name)
+      send_file zipped_file_path,
+                type: 'application/zip', 
+                filename: file_name, 
+                diposition: 'attachment'
 
+    else  
+      flash[:alert] = "You need to send a zip file"
+      redirect_to summary_reviews_path
     end
   end
 end
