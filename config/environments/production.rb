a4f0a25557fb8c0fc48ebd9e689521c3785866a4
Thao Nguyen <nguyen_huynh_thanh_thao@optimizer.co.jp>
2025-02-28 14:49:02 +0700

    Allow Render domain in config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "summary-review.onrender.com"
+  config.hosts = ["summary-review.onrender.com"]
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
