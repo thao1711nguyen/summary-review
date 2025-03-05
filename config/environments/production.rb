e0991a8f6e9ef19971549bca3d6eedbd5f45e388
Thao Nguyen <nguyen_huynh_thanh_thao@optimizer.co.jp>
2025-03-05 10:59:36 +0700

    fix block host error again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts.clear
+  config.hosts << /summary-review\.onrender\.com/
 
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
