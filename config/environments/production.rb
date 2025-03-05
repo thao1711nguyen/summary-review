8f1e5287272b4a18cea4c034ea6a0deabb6f2b89
Thao Nguyen <nguyen_huynh_thanh_thao@optimizer.co.jp>
2025-03-05 10:45:23 +0700

    try to fix block hosts error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,11 +93,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts.clear
+  # config.hosts.clear
 
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`P
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
